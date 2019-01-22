df4d1ecb2c8bb33ee23faf43fa91b3eb60180c99
Jonas Bohmann <bohmannjonas@gmail.com>
2019-01-22 13:48:40 +0100

    Add warning for ties in calculation
    Change behavior of catching ValueErrors, now allows to re-enter invalid input

--- dhondt.py
+++ dhondt.py
@@ -1,5 +1,4 @@
 # ======== D'hondt calculator ========
-import time
 
 def main():
 
@@ -8,37 +7,40 @@ def main():
     wonSeats = {}
     allocatedSeats = 0
 
-    print("==== D'HONDT CALCULATOR ====")
+    print("==== D'HONDT CALCULATOR ====\n")
+
+    while True:
+        try:
+            seats = int(input("How many seats are up for election?\n"))
+            break
+        except ValueError:
+            print("The amount of seats has to be a positive integer!")
 
 
-    try:
-        seats = int(input("How many seats are up for election?\n"))
-    except ValueError:
-        print("The amount of seats has to be a positive integer.\nThe program will now quit.")
-        time.sleep(3)
-        quit()
+    while True:
+        try:
+            amountOfParties = int(input("How many parties participated in the election?\n"))
+            break
+        except ValueError:
+            print("The amount of parties has to be a positive integer!")
 
-    try:
-        amountOfParties = int(input("How many parties participated in the election?\n"))
-    except ValueError:
-        print("The amount of parties has to be a positive integer.\nThe program will now quit. ")
-        time.sleep(3)
-        quit()
+
 
     for x in range(amountOfParties):
-        try:
-            party = str(input("Name of Party: "))
-        except ValueError:
-            print("The name of a party has cannot be empty.\nThe program will now quit.")
-            time.sleep(3)
-            quit()
+        while True:
+            try:
+                party = str(input("Name of Party #" + str(x+1) + ": "))
+                break
+            except ValueError:
+                print("The name of a party has cannot be empty!")
 
-        try:
-            inputVotes = int(input("Amount of Votes for " + party + ": "))
-        except ValueError:
-            print("The amount of votes of a party has to be a positive integer.\nThe program will now quit.")
-            time.sleep(3)
-            quit()
+
+        while True:
+            try:
+                inputVotes = int(input("Amount of Votes for " + party + ": "))
+                break
+            except ValueError:
+                print("The amount of votes of a party has to be a positive integer!")
 
         quota[party] = inputVotes
         votes[party] = inputVotes
@@ -50,8 +52,19 @@ def main():
     while allocatedSeats < seats:
         winner = max(quota.values())
         winnerKeys = [key for (key, value) in quota.items() if value == winner]
+
+        # If there's a tie between the quotas of at least 2 parties, the seat goes to the first party
         winnerKey = winnerKeys[0]
-        print(str(winnerKey) + " wins seat #" + str(allocatedSeats + 1))
+
+        # Gives out warning if there's a tie between at least 2 quotas in the calculation
+        if len(winnerKeys) > 1:
+            print("WARNING! - There was a tie between " + str(winnerKeys) + ". The seat went to the party that you "
+                  "entered first in the beginning.")
+
+        # Result output
+        print(str(winnerKey) + " wins seat #" + str(allocatedSeats + 1) + " with the highest quota of " + str(quota[winnerKey]))
+
+        # Give seat to winner & calculate new quota
         wonSeats[winnerKey] = wonSeats[winnerKey] + 1
         quota[winnerKey] = votes[winnerKey] / (wonSeats[winnerKey] + 1)
         allocatedSeats += 1
@@ -60,9 +73,11 @@ def main():
     for key in wonSeats:
         print("Amount of seats for %s: %s" % (key, wonSeats[key]))
 
-    print("\nThe program will quit after 10 seconds.")
-    time.sleep(10)
-    quit()
+
+    # For windows users who use the python launcher
+    x = input("\nPress 'ENTER' to exit... ")
+    if len(x) >= 0:
+        quit()
 
 if __name__ == "__main__":
     main()
